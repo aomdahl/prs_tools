2edd8a0c366b07bb9fb0e4092e27693b576498f3
Ashton Omdahl <aomdahl@gmail.com>
2019-08-21 11:09:54 -0400

    Most updated version, looks like it works through

--- calc_prs.py
+++ calc_prs.py
@@ -32,17 +32,13 @@ def getStatHeaders(setting):
 def loadSummaryStats(sum_path, sum_format):
     h = getStatHeaders(sum_format)
     ss = pd.read_csv(sum_path, sep = ' ', index_col = False, 
-                     dtype={h[CHR]:'category', h[POS]:'int32', h[ALT]: 'category', h[REF]:'category', h[BETA]:'float64', h[PVAL]:'float64', })
+                     dtype={h[CHR]:'category', h[POS]:'int32', h[ALT]: 'category', h[REF]:'category', h[BETA]:'float64', h[PVAL]:'float64'})
     ss[REFID] = ss[h[CHR]].astype(str) + ":" + ss[h[POS]].astype(str)
     return ss, h
 
 def loadGenoVars(snp_file):
-    #snp_file = plinkio.PlinkFile(snp_file)
-    #put it into a format we can do something with... We actually really only need the bim file it looks like.  
-    #bim_file = pd.read_csv(sum_path, sep = '\t', index_col = 1, header = None, Names = [CHR, REFID, "centi", POS, "allele1", "allele2"]) #1 is usually the minor
-    #TODO: possible speedup, by choosing index column for quick lookup? (as above)
-    #bim_file = pd.read_csv(sum_path, sep = '\t', header = None, names = [CHR, REFID, "centi", POS, "allele1", "allele2"], dtype={CHR:'int32', REFID:'str', 'centi':'int32', POS:'int32', "allele1": 'category', 'allele2':'category'}, skiprows=list(range(0,23))) 
     bim_file = pd.read_csv(snp_file, sep = '\t', header = None, names = [CHR, POS, REFID, REF, ALT], dtype={CHR:'category', REFID:'str', POS:'int32', ALT: 'category', REF:'category'}, skiprows = list(range(0,24))) 
+    return bim_file
 
     #Speed up the read in by specifying the type
     #Omit the first 23 lines- they are header information
@@ -55,27 +51,16 @@ def filterSNPs(pval, maf, ss, vars, h):
     ss_new, vars_new = getIntersectingData(ss[(ss[h[PVAL]] <= pval) & (ss[h[CHR]] != "X")], vars)
     return ss_new[REFID], ss_new, vars_new
 
-
-def calculatePRS(ss, vars,h, geno_mat, args):
-    
+def calculatePRS(ss, geno_mat):
+    id_list = ss[REFID].values
+    snp_matrix = geno_mat.loc[id_list]
     #do the necessary math: choose all the right rows.
     #Assuming the alt are the affect alleles.. that is what we assume. Was true in the small number of cases I looked at.
-    dat = 2 - geno_mat.iloc[,6:] #Columns from 6 on
-    betas = (ss[BETA].values).T
+    dat = (2-snp_matrix.iloc[:,5:]).values
+    betas = stats_filtered[BETA].values
     scores = np.matmul(betas, dat)
-    """
-    #This current way of doing it is a bit janky, but gets the job done.
-    effect_alleles = ss_k[EFFAL]
-    #Alternate way
-    ss_k["allele_match"]  = (effect_alleles == ss_k['allele1']).astype('int32') + (effect_alleles == ss_k['allele2']).astype('int32')
-
-    #ss_k["allele_count"] = [0 if (a != e and b!= e) else 2 if (a == e and b == e) else 1 for a,b, e in zip(ss_k['allele1'], ss_k['allele2'], s_k[EFFAL])]   
-    #I don't know about the speed of this command, it seems like it will be pretty slow.
-    #USING PYPLINK
-    """
-    #return np.dot(ss[h[BETA]].values, vars_keep)
     return scores
-    #wouldn't we want something scaled- as in values that have more SNPs?
+
 
 #TODO: This is slow, probably because of the sorting. Come up with a way to maintain order and speed.
 def getIntersectingData(ss, vars):
@@ -86,27 +71,32 @@ def getIntersectingData(ss, vars):
     return ss[ss[REFID].isin(vars_keep[REFID])].sort_values(by=REFID), vars_keep, 
     
 
-def plinkToMatrix(snp_keep, ss, vars, args):
+
+def plinkToMatrix(snp_keep, args):
     #Write out the list of SNPs to keep
     snp_order = list(snp_keep.values)
     list_dir = writeSNPIDs(snp_keep)
     #Make the new matrix
     from subprocess import call 
-    call(["plink2", "--pfile", args.plink_snps, "--not-chr", "X", "--extract", list_dir, "--out", "mat_form_tmp", "--export", "A-transpose"]) 
-    return pd.read_csv("mat_form_tmp.traw", sep = "\t")
-    #we are gonna do it all with plink2. BOwho
-    #Note that the order is as given by snp_order.
-    #Run plink, write out as a matrix (now much smaller hopefully)
-    #Call outsite of python.
-    #Read in the matrix and associated files (ah... the order will be redone so we will need to anyway. Shoot. Wasted resources. Unless we can finalize resources.
+    call(["plink2", "--pfile", args, "--not-chr", "X", "--extract", list_dir, "--out", "mat_form_tmp", "--export", "A-transpose"]) 
+    return pd.read_csv("mat_form_tmp.traw", sep = "\t", index_col="SNP")
+
+def getPatientIDs(snp_matrix):
+    tnames = list(snp_matrix.columns)[5:]
+    return [int(i.split("_")[0]) for i in tnames]
 
 
-def writeScores(s, destination):
+def writeScores(scores, ids, destination):
+    with open(destination, 'w') as ostream:
+        ostream.write("ID\tScore\n")
+        for s, id, in zip(scores, ids):
+            ostream.write(str(s) + '\t' + str(id) + '\n')
+
     return
 
 def writeSNPIDs(ids):
     ids.to_csv("snp_ids.tmp", header = False, index = False, sep = '\t') #(should have 2 columns of the family/infamily ids, which appear to be the same.)
-    return "snp_ids.tmp"    
+    return "snp_ids.tmp"
 
 if __name__ == '__main__':
 
@@ -128,13 +118,11 @@ if __name__ == '__main__':
     snp_matrix = plinkToMatrix(snp_list, stats_filtered, variants_filtered)
     #stats was output of filterSNPs
     scores = calculatePRS(stats_filtered, header, snp_matrix)
-    writeScores(scores, args.output)
+    patient_ids  = getPatientIDs(snp_matrix)
+    writeScores(scores, patient_ids, args.output)
 
     #Match SNPs by address and/or reference ID.
-    print("Success?")
-    print(stats)
-
-    input()
+    print("scores written out to", args.output)
     #add option to accomodate large size files with limited memory- chunk it.
 
     #Load the summary stats into memory quickly.
