04ada50aabdc29896bab2296da6da5aad82696ae
Ashton Omdahl <aomdahl@gmail.com>
2019-08-26 11:45:41 -0400

    Added multi-pvalue functionality

--- calc_prs.py
+++ calc_prs.py
@@ -148,48 +148,38 @@ def writeSNPIDs(ids):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description = "Basic tools for calculating rudimentary Polygenic Risk Scores (PRSs). This uses PLINK bed/bim/fam files and GWAS summary stats as standard inputs. Please use PLINK 2, and ensure that there are no multi-allelic SNPs (PLINK biallelic strict))
+    parser = argparse.ArgumentParser(description = "Basic tools for calculating rudimentary Polygenic Risk Scores (PRSs). This uses PLINK bed/bim/fam files and GWAS summary stats as standard inputs. Please use PLINK 2, and ensure that there are no multi-allelic SNPs")
     parser.add_argument("-snps", "--plink_snps", help = "Plink file handle for actual SNP data (omit the .extension portion)")
     parser.add_argument("-ss", "--sum_stats", help = "Path to summary stats file. Be sure to specify format if its not DEFAULT. Assumes tab delimited")
     parser.add_argument("--ss_format", help = "Format of the summary statistics", default = "SAIGE", choices = ["DEFAULT", "SAIGE"])
     parser.add_argument("-p", "--pval", default = 5e-8, type = float, help = "Specify what pvalue threshold to use.")
+    parser.add_argument("--pvals", help= "Use this if you wish to specify multiple at once, in a list separated by commas")
     parser.add_argument("--maf", default = 0.01, type = float, help = "Specify what MAF cutoff to use.")
     parser.add_argument("-o", "--output", default = "./prs_" + str(date.today()), help = "Specify where you would like the output file to be written and its prefix.")
     parser.add_argument("--train", action = "store_true", help = "Select this if training a PRS model. This will save a list of the SNPs we include in the test, and can also draw some plots (?)")
 
     args = parser.parse_args()
+    pvals = args.pval
+    if args.pvals:
+        pvals = [float(x) for x in (args.pvals).split(",")]
+        
     print("Loading summary statistics and relevant genotype information....")
     stats, header = loadSummaryStats(args.sum_stats, args.ss_format)
     variants = loadGenoVars(args.plink_snps + ".pvar")
     print("Filtering SNPs...")
-    snp_list, stats_filtered, variants_filtered = filterSNPs(args.pval, args.maf, stats, variants, header) #TODO: makes sure the variatns are in order.
-
-    #Run plink filtering on the bed file, and then load it into memory
-    print("Building a PLINK matrix...")
-    snp_matrix = plinkToMatrix(snp_list, args.plink_snps)
-    #stats was output of filterSNPs
-    print("Calculating PRSs")
-    #stats_qc, snp_qc = qualityControl(stats_filtered, snp_matrix)
-    scores = calculatePRS(stats_filtered, snp_matrix)
-    patient_ids  = getPatientIDs(snp_matrix)
-    writeScores(scores, patient_ids, args.output)
-
-    #Match SNPs by address and/or reference ID.
-    print("scores written out to", args.output)
-    #add option to accomodate large size files with limited memory- chunk it.
-
-    #Load the summary stats into memory quickly.
-
-    #Make any checks or corrections that are needed
-    #Check for ambiguous SNP changes- they should be removed
-
-
-    #MAke sure 
-
-    #Add an option to do clumping if you want?
-
-    #Make another script for easy visualization of the outpu.
-    
-    
+    for pval in pvals:
+        snp_list, stats_filtered, variants_filtered = filterSNPs(pval, args.maf, stats, variants, header) #TODO: makes sure the variatns are in order.
+        #Run plink filtering on the bed file, and then load it into memory
+        print("Building a PLINK matrix for pval", str(pval), "...")
+        snp_matrix = plinkToMatrix(snp_list, args.plink_snps)
+        #stats was output of filterSNPs
+        print("Calculating PRSs")
+        #stats_qc, snp_qc = qualityControl(stats_filtered, snp_matrix)
+        scores = calculatePRS(stats_filtered, snp_matrix)
+        patient_ids  = getPatientIDs(snp_matrix)
+        writeScores(scores, patient_ids, args.output + "_" + str(pval) + ".tsv")
+        #Match SNPs by address and/or reference ID.
+        print("Scores written out to", args.output + "_" + str(pval) + ".tsv")
+        #add option to accomodate large size files with limited memory- chunk it.
 
 
