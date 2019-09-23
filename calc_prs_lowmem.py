2b82a3051439c8910c85ec3e095b48df80d38beb
Ashton Omdahl <aomdahl@gmail.com>
2019-09-23 15:07:51 -0400

    Debugging and getting things working with the lowmem approach. Should work now

--- calc_prs_lowmem.py
+++ calc_prs_lowmem.py
@@ -11,7 +11,7 @@ from datetime import date
 import sys
 from subprocess import call 
 import os
-
+import time
 #General reference things
 CHR = "chr"
 POS = "pos"
@@ -27,11 +27,6 @@ ALT = "ALT" #Alternate allele
 INDEX = 0
 B = 1
 
-def getStatHeaders(setting):
-    if setting == "SAIGE":
-        hl = "#CHROM POS ID REF ALT ac af num_cases num_controls beta sebeta Tstat pval pval_SAIGE_NoSPA Is_Converged varT varTstar".split(" ")
-        return {CHR: hl[0], POS:hl[1], REFID: hl[2], EFFAL: hl[4], EFFALFREQ:hl[6], BETA:hl[9], PVAL:hl[12], ALT:hl[4], REF:hl[3]}
-        #Can certainly add more later, but this is a fine start.
         
 def readInCol(fin):
     ret_list = list()
@@ -48,11 +43,13 @@ def readInSummaryStats(s_path):
     
     return ss#, readInCol(ids_path)
 
-def filterSumStats(pvals,ss,):
+def filterSumStats(pvals,ss):
     pvals_ref = dict() #dictionary containing a pointer for each p value
     for p in pvals:
-        samp = ss[(ss[PVAL] <= p)]
-        pvals_ref[p] = [samp.index.tolist(), samp[PVAL].values] #Get just the pvalues and the indices.
+        samp = ss[(ss[PVAL] <= float(p))]
+        pvals_ref[p] = [samp.index.tolist(), samp[BETA].values] #Get just the pvalues and the indices.
+        #print("Scores as extracted from sum stats....")
+        #print(pvals_ref[p][0], pvals_ref[p][1])
     return pvals_ref
         
 def prepSummaryStats(geno_ids, sum_path, pval_thresh):
@@ -60,8 +57,6 @@ def prepSummaryStats(geno_ids, sum_path, pval_thresh):
     This function uses awk to filter the summary stats by pvalue, and then reads them into a dask data structure.
     """
     #If the id is in the geno_ids file, and it passes the p-value threshold, keep it
-    #With the most recent 9/13 update, this is superfluousi
-    #In fact, we should just read this into a dask and call it a day
     command = "awk '($7 <= " + str(pval_thresh) + ") {print $0}' " + sum_path + " > ss.tmp"
     #command = '''awk '(FNR == NR) {a[$1];next} ($1":"$3 in a && $7 <= ''' + str(pval_thresh) + ") {print $0}' " + geno_ids + " " + sum_path + " > ss.tmp"
     #command = "awk '(FNR == NR) {a[$1];next} ($1 in a && $7 <= " + str(pval_thresh) + ") {print $0}' " + geno_ids + " " + sum_path + " > ss.tmp"
@@ -86,11 +81,8 @@ def prepSNPIDs(snp_file, ss_file, ss_type, ambig):
 
     #Remove all the header lines, just get what we need.
     if not os.path.isfile('geno_ids.f'):
-        #command = "awk '(!/##|ID/ && substr($3,1,2) !~ /X:/) {print $3 }' " + snp_file + ".pvar > geno_ids.f"
-        #Generate your own pvar file and use that, since the IDs are non-unique >_<
-        #Still working on this (below_)
         
-        command = '''awk '(!/##/ && $1 !~ /X/) {print $1"\t"$2"\t"$3":"$4":"$5"\t"$4"\t"$5}' ''' + snp_file + ".pvar " + " | sed '1 s/ID:REF:ALT/ID/' > local_geno.pvar"
+        command = '''awk '(!/##/ && $1) {print $1"\t"$2"\t"$3":"$4":"$5"\t"$4"\t"$5}' ''' + snp_file + ".pvar " + " | sed '1 s/ID:REF:ALT/ID/' > local_geno.pvar"
         #command = '''awk '(!/##|ID/ && $1 !~ /X/) {print $3":"$4":"$5}' ''' + snp_file + ".pvar > geno_ids.f"
         call(command, shell = True)
         command = "tail -n +2 local_geno.pvar | cut -f 3 > geno_ids.f"
@@ -101,20 +93,54 @@ def prepSNPIDs(snp_file, ss_file, ss_type, ambig):
             command = '''awk '(FNR == NR) {a[$1];next} (FNR == 1 && NR == 2) {next;} ($1":"$2":"$4":"$5 in a) {print $1":"$2":"$4":"$5, $4,$5,$10,$11,$12,$13}' geno_ids.f ''' + ss_file + " > ss_filt.f"
             #command = '''awk '(FNR == 1) {next;} {print $1":"$2, $4,$5,$10,$11,$12,$13}' ''' + ss_file + " > ss_filt.f"
             call(command, shell = True)
-            #call(["awk", """'(FNR == 1) {next;} {print $1":"$2, $4,$5,$10,$11,$12,$13}'""",ss_file, ">", "ss_filt.f"]) #TODO: find ways to speed up this step and downstream ones, maybe filter out X chrom?
+            
+            #reset the ids we use downatream
+            command = "cut -f 1 -d ' ' ss_filt.f > geno_ids.f"
+            call(command, shell = True)
         else:
             print("The type of ss file hasn't been specified. Please specify this.")
 
     return "local_geno.pvar","geno_ids.f", "ss_filt.f"
 
+def scoreCalculation(geno, betas):
+    #print("multiplying the following....", geno[:3], betas[:3])
+    #return np.dot(geno[:3], betas[:3])
+    return np.dot(geno, betas)   
 
+def linearGenoParse(snp_matrix, snp_indices,pvals, scores, patient_ids):
+    with open(snp_matrix, 'r') as istream:
+        counter = 0
+        line = istream.readline().strip()
+        while line:
+        #for line in istream:
+            line = line.strip()
+            if counter == 0:
+                line = istream.readline().strip()
+                counter += 1
+                continue
+            dat = np.array(line.split(), dtype = np.float64)
+            patient_ids.append(dat[0])
+            dat = dat[6:] #Just the numberic values at the end
+            for p in pvals:
+                #print("P value:", p)
+                #print("patient number:", int(patient_ids[-1]))
+                scores[p].append(scoreCalculation(dat[snp_indices[p][INDEX]],snp_indices[p][B]))
+            counter += 1
+            if counter % 100 == 0:
+                print("Currently at patient", counter)
+            line = istream.readline()
+            #if counter % 10 == 0 :
+            #    break
+    return scores, patient_ids
 
-def calculatePRS(ss, snp_matrix):
-    dat = (2-snp_matrix.iloc[:,6:])
-    #dat = (2-snp_matrix.loc[:,snp_list]) #this should yield an n x m array, n patients by m snps
-    betas = ss[BETA] #Select the    Betas, should be a list of length m
-    scores = np.matmul(betas, dat)
-    return scores
+def calculatePRS(pvals, snp_matrix, snp_indices):
+
+    scores = dict() #where the scores get stored
+    for p in pvals:
+        scores[p] = list()
+    patient_ids = list() #store the patient's ids...
+    scores, patient_ids = linearGenoParse(snp_matrix, snp_indices,pvals, scores, patient_ids)
+    return scores, patient_ids
 
 def sameAlleleAssesment(s1, s2):
     t = (s1 != s2).sum()
@@ -125,24 +151,6 @@ def sameAlleleAssesment(s1, s2):
         print("^Unusual case")
         return False
 
-
-#TODO: This is slow, probably because of the sorting. Come up with a way to maintain order and speed.
-#Checked functionality theoretically, seems to work.
-def getIntersectingData(ss, vars):
-    """
-    @param ss: the summary statistics file, in a filtered dask
-    @param vars: the read in pvar file, all the SNPs for which we have information.
-    """
-    #Because ss is a dask structure, we should select IDs from vars instead.
-    #Also note that the dask is currently indexed by ID, so this should be FAST
-    #id_keep = ss[REFID].values
-    #select out the values in vars that are in the vars_keep list (make sure IN ORDER)
-    #Dask cannot sort values
-    ss_filt = ss.loc[vars.index]
-    #vars_keep = vars[vars[REFID].isin(id_keep)].sort_values(by=REFID) #Remember, if its a 3 we ignore it- it means the data isn't available for them.
-    #return ss[ss[REFID].isin(vars_keep[REFID])].sort_values(by=REFID), vars_keep 
-    return ss_filt, vars_keep
-    
 def plinkToMatrix(snp_keep, args, local_pvar):
     """
     @param snp_keep is the path to the file with the SNP list. Currently just a single column, TODO check if this will work for plink filtering.
@@ -153,19 +161,7 @@ def plinkToMatrix(snp_keep, args, local_pvar):
     else:
         call(["plink2", "--pfile", args, "--pvar", local_pvar, "--not-chr", "X", "--extract", snp_keep, "--out", "mat_form_tmp", "--export", "A", "--max-alleles", "2"]) 
         print("New plink matrix made!")
-    #ret = dask.dataframe.read_csv("mat_form_tmp.traw", sep = "\t") #Changed this to a dask, let's see if its faster...
-    #No need to set an index- everything is in order!
-    """
-    import time
-    start = time.time()
-    ret = pd.read_csv("mat_form_tmp.traw", sep = '\t', index_col = "SNP")   
-    end = time.time()
-    print("read in time:", str(end-start))
-    print("In memory size")
-    ret.info(memory_usage='deep') 
-    #ret = ret.set_index("SNP")
-    ret = ret.rename(columns={"COUNTED":REF})
-    """
+        #input()
     return "mat_form_tmp.raw"
 
 def getPatientIDs(snp_matrix):
@@ -175,16 +171,16 @@ def getPatientIDs(snp_matrix):
 
 def writeScores(scores, ids, destination):
     with open(destination, 'w') as ostream:
-        p_string = ""
+        p_string = "ID\t"
         num = 0
         for p in scores:
             p_string = p_string + str(p) + '\t'
             num = len(scores[p]) #number of patients
-        ostream.write(p_string[:-1])
+        ostream.write(p_string[:-1] + '\n')
         for i in range(0, num):
-            write_s = ""
+            write_s = str(int(ids[i])) + '\t'
             for p in scores:
-                write_s = str(scores[p][i]) + '\t'
+                write_s = write_s + str(scores[p][i]) + '\t'
             write_s = write_s[:-1] + '\n'
             ostream.write(write_s)
 
@@ -211,7 +207,6 @@ def datasetsAlign(snps, ss, snp_list):
     #print("Same alt alleles!")
     #assert(sameAlleleAssesment(check[REF], snp_matrix[REF]))
     #print("Same REF alleles!")
-    
 
 if __name__ == '__main__':
 
@@ -227,6 +222,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     pvals = [args.pval]
 
+    start = time.time()
     if args.pvals:
         pvals = [float(x) for x in (args.pvals).split(",")]
         
@@ -237,36 +233,13 @@ if __name__ == '__main__':
     snp_matrix = plinkToMatrix(geno_ids, args.plink_snps, local_pvar) 
     snp_indices = filterSumStats(pvals,stats_complete)
     #Now do it 
-    scores = dict.fromkeys(pvals, []) #should make a dictionary of pvalue lists.
-    patient_ids = list()
-    print("reading the snps in....")
-    with open(snp_matrix, 'r') as istream:
-        counter = 0
-        line = istream.readline().strip()
-        while line:
-        #for line in istream:
-            line = line.strip()
-            if counter == 0:
-                first_line = line
-                line = istream.readline().strip()
-                counter += 1
-                print ("header line extracted")
-                continue
-            dat = np.array(line.split(), dtype = np.float64)
-            patient_ids.append(dat[0])
-            dat = dat[6:] #Just the numberic values at the end.
-            for p in pvals:
-                vals = dat[snp_indices[p][INDEX]] #Select out just those indices
-                score = np.dot(vals, snp_indices[p][B])
-                scores[p].append(score)
-            counter += 1
-            if counter % 50 == 0:
-                print("Currently at patient", counter)
-            line = istream.readline()
+    print("Parsing the genotype file...")
+    scores, patient_ids = calculatePRS(pvals, snp_matrix, snp_indices)
+    writeScores(scores, patient_ids, args.output + ".tsv")
+    #Match SNPs by address and/or reference ID.
+    print("Scores written out to", args.output + ".tsv")
+    stop = time.time()
+    print("Total runtime:", str(stop - start))
 
-        writeScores(scores, patient_ids, args.output + "_" + str(pval) + ".tsv")
-        #Match SNPs by address and/or reference ID.
-        print("Scores written out to", args.output + "_" + str(pval) + ".tsv")
-        #add option to accomodate large size files with limited memory- chunk it.
 
 
