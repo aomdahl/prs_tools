4987de19b349f63e6a29b388f5007f24f4555d58
Ashton Omdahl <aomdahl@gmail.com>
2020-08-27 10:04:23 -0400

    run_prs_template.sh

--- run_prs_template.sh
+++ run_prs_template.sh
@@ -9,21 +9,21 @@ SUMSTAT="/work-zfs/abattle4/ashton/prs_dev/dbp_ss_neal/4079_irnt.gwas.imputed_v3
 LDREF="/work-zfs/abattle4/ashton/prs_dev/1000genomes_refLD/ref"
 PHENO="/work-zfs/abattle4/marios/HF_GWAS/cohorts/CHS/merged_genotypes/CNV370v1/preimpute/race1/unrelated/pheno_mendrand.txt"
 #To run the tool in just one step:
-python $SC/prs_tools/calc_prs_lowmem.py -snps $GENO -ss $SUMSTAT --pvals 1,0.5,0.3,0.1,0.01,0.001,1e-4,1e-6,5e-8  -o results_jan9 -pv 2 --reset_ids --ss_format NEAL --align_ref --clump --ld_ref $LDREF --debug
+python $SC/prs_tools/calc_prs.py -snps $GENO -ss $SUMSTAT -o results_jan9 -pv 2 --reset_ids --ss_format NEAL --align_ref --clump --ld_ref $LDREF  --no_na
  
 
 #You can separately run the preprocessing to make sure everything is working alright, but this isn't necessary
-python $SC/prs_tools/calc_prs_lowmem.py -snps $GENO -ss $SUMSTAT --pvals 0.1,0.01,0.001,1e-4,1e-6,5e-8  -o results_jan9 -pv 2 --reset_ids --ss_format NEAL --align_ref --clump --ld_ref $LDREF --only_preprocess 
+python $SC/prs_tools/calc_prs.py -snps $GENO -ss $SUMSTAT  -o results_jan9 -pv 2 --reset_ids --ss_format NEAL --align_ref --clump --ld_ref $LDREF --preprocess_only 
     #You can check the outputs from above to see if they look right. 
 
 #Now run the actual calculation step
-python $SC/prs_tools/calc_prs_lowmem.py -snps new_plink -ss filtered_NEAL.ss --pvals 1,0.5,0.1,0.01,0.001,1e-4,1e-6,5e-8  -o results_jan9 -pv 2 --reset_ids --ss_format DEFAULT --prefiltered_ss --preprocessing_done
+python $SC/prs_tools/calc_prs.py -snps new_plink -ss filtered_NEAL.ss --pvals 1,0.5,0.1,0.01,0.001,1e-4,1e-6,5e-8  -o results_jan9 -pv 2 --reset_ids --ss_format DEFAULT --prefiltered_ss --preprocessing_done
     #This should generate your output file! Note that many of the file names changed, since they are filtered and printed out for convenience inthe step above. In the future, I may set the outputs just into a serialized file for speed, but this version is convenient for debugging.
 
 #To asses the performance of the scoring:
 PA="/work-zfs/abattle4/ashton/prs_dev/prs_tools"
 module load gcc/5.5.0
 module load R
-Rscript $PA/prs_asses_lowmem.R  --prs_results results_jan9.tsv --pheno $PHENO --output figs_chs_race1 --risk_trait DBP --covars gender
+Rscript $PA/prs_asses.R  --prs_results results_jan9.tsv --pheno $PHENO --output figs_chs_race1 --risk_trait DBP --covars gender
     #this will generate a plot showing the r2 of scores with DBP, as well as a matrix, corr_count.tsv, that shows the raw r2 scores
 
